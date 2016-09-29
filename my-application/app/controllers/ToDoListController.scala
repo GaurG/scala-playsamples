797c7805b3ba68b0df90c48fd246728964c4721d
Gaurav Gaur <gaurav.gaur@digital.hmrc.gov.uk>
2016-09-29 23:02:49 +0100

    modified to do view

--- my-application/app/controllers/ToDoListController.scala
+++ my-application/app/controllers/ToDoListController.scala
@@ -8,8 +8,7 @@ import play.api.mvc._
 class ToDoListController @Inject extends Controller {
 
     def toDoList = Action {
-        //Ok(views.html.toDo)
-        val toDoList = List("Code for different HTTP requests such as get/post/delete/put", "")
+        val toDoList = List("Code for different HTTP requests such as get/post/delete/put")
          Ok(views.html.toDo(toDoList))
     }
 
